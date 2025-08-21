6595fcc14a9812ea626fdbc80802057203fd4794
Márcio Marques <marcmarq2@yahoo.com.br>
2025-08-20 22:48:25 -0300

    Correções e sincronização realizada

--- STEPPER_02-r01/STEPPER_02-r01.ino
+++ STEPPER_02-r01/STEPPER_02-r01.ino
@@ -1,6 +1,5 @@
 // Controle de motor - CNC Shield
-// Versão 02 - Revisão 01 - Introduzido em 20/08/2025 - TESTE!!!
-// Novo comentário - Alterado!!
+// Versão 02 - Revisão 01
 // Definição dos pinos 
 #define stepPin 2 //Passo eixo X
 int dirPin = 5; // Direção eixo X
